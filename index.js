b54382017b183f659b2e8c2e90ccb1c33b132ee5
Emilio Villuendas <emiliovillu@gmail.com>
2017-11-01 16:43:50 +0100

    add mode session

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path')
 const uuid = require('uuid/v4')
 const app = express()
 const routes = require('./routes')
+const session = require('express-session')
+
 
 let tasks = []
 
@@ -14,8 +16,18 @@ app.set('views', path.resolve(__dirname, 'views'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(routes)
+app.use(session({
+  secret: 'puede ser lo que sea',
+  resave: false,
+  saveUninitialized: true
+}))
 
+app.use((req, res, next) => {
+  req.session.tasks = req.session.tasks || []
+  next()
+})
+
+app.use(routes)
 
 app.listen(3000)
 console.log('Listening on PORT 3000...')
--- routes/handlers/_addTask.js
+++ routes/handlers/_addTask.js
@@ -1,8 +1,9 @@
 const { addTask } = require('../../services/todo')
 
 function _addTask(req ,res){
-  const {task} = req.body
+  const { task } = req.body
   addTask(task)
+  req.session.tasks.push(task)
   res.redirect('/')
 }
 module.exports = _addTask
